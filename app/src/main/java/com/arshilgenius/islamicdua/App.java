7afc6d1f943df5c1c2f3f69a87afdae0f1b3d12d
Arshil Khan <Arshilgenius@gmail.com>
2017-06-15 00:27:27 +0530

    changes applied

---
+++ app/src/main/java/com/arshilgenius/islamicdua/App.java
@@ -0,0 +1,18 @@
+package com.arshilgenius.islamicdua;
+
+import android.app.Application;
+
+/*import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.iconics.Iconics;*/
+
+
+// This file might be required once I need to update the Android Iconics library (https://github.com/mikepenz/Android-Iconics)
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Iconics.registerFont(new FontAwesome());
+        // Iconics.registerFont(new GoogleMaterial());
+    }
+}
